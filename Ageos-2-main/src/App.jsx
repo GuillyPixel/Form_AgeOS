a2fbbb05321654b4ff2367e636a6d47e0a67dfc0
Guilherme Martins Fernandes <guillherme10mar@gmail.com>
2024-10-20 01:29:45 -0300

    pt01

---
+++ Ageos-2-main/src/App.jsx
@@ -0,0 +1,14 @@
+import Formulario from "./Components/Fomulario/Form"
+import './stylesheets/index.scss'
+
+export default function App() {
+
+  return (
+    <>
+     <div>
+      <Formulario/>
+      </div> 
+    </>
+  )
+}
+
